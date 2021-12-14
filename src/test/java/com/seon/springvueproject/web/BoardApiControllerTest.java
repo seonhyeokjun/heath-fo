57324d9159e52bd0d4e82012d1b92ef1212a606d
seonhyeokjun <tjsrmatn@naver.com>
2021-12-14 22:43:32 +0900

    TDD 등록된다 로직 수정

--- src/test/java/com/seon/springvueproject/web/BoardApiControllerTest.java
+++ src/test/java/com/seon/springvueproject/web/BoardApiControllerTest.java
@@ -17,18 +17,29 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -41,7 +52,7 @@ public class BoardApiControllerTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
-    private BoardRepository postsRepository;
+    private BoardRepository boardRepository;
 
     @Autowired
     private WebApplicationContext context;
@@ -58,37 +69,48 @@ public class BoardApiControllerTest {
 
     @After
     public void tearDown(){
-        postsRepository.deleteAll();
+        boardRepository.deleteAll();
     }
 
     @Test
     @WithMockUser(roles="USER")
-    public void Posts_등록된다() throws Exception {
+    public void Board_등록된다() throws Exception {
         // given
         String title = "title";
         String content = "content";
         BoardSaveRequestDto requestDto = BoardSaveRequestDto.builder()
                 .title(title)
-                .content(content)
                 .author("author")
+                .content(content)
+                .hit(0)
                 .build();
+
+        MockMultipartFile key = new MockMultipartFile("key",
+                "",
+                "application/json", "{\"title\":\"title\", \"author\":\"author\", \"content\":\"content\", \"hit\":0}".getBytes(StandardCharsets.UTF_8));
 
         String url = "http://localhost:" + port + "/api/board";
 
         //when
-        mvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+//        mvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_UTF8)
+//                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+//                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders.multipart(url)
+                .part(new MockPart("key", key.getBytes())))
+                .andDo(print())
                 .andExpect(status().isOk());
 
-        List<Board> all = postsRepository.findAll();
+        List<Board> all = boardRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
-    public void Posts_수정된다(){
+    @WithMockUser(roles="USER")
+    public void Board_수정된다() throws Exception{
         // given
-        Board savedPosts = postsRepository.save(Board.builder()
+        Board savedPosts = boardRepository.save(Board.builder()
                 .title("title")
                 .content("content")
                 .build());
@@ -102,17 +124,16 @@ public class BoardApiControllerTest {
                 .content(expectedContent)
                 .build();
 
-        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
-        HttpEntity<BoardUpdateRequestDto> requestDtoHttpEntity = new HttpEntity<>(requestDto);
+        String url = "http://localhost:" + port + "/api/board/" + updateId;
 
-        // when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestDtoHttpEntity, Long.class);
+        //when
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-        List<Board> all = postsRepository.findAll();
+        List<Board> all = boardRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
@@ -120,7 +141,7 @@ public class BoardApiControllerTest {
     @Test
     public void Posts_불러온다(){
         // given
-        Page<BoardResponseDto> postsList = postsRepository.findAllDesc(PageRequest.of(0, 2));
+        Page<BoardResponseDto> postsList = boardRepository.findAllDesc(PageRequest.of(0, 2));
         String url = "http://localhost:" + port + "/api/v1/posts/list";
     }
 }
